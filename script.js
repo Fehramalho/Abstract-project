e16e383de152a28612ba1c9c7a7ff6f62284fc03
Fehramalho <ramalho543@gmail.com>
2024-07-27 12:35:39 +0100

    vamos ao java

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Seleciona os elementos dos botões e do menu
-    var btnMenu = document.querySelector('.btn-menu');
-    var btnClose = document.querySelector('.btn-close');
-    var menu = document.querySelector('.menu');
+    var lupa = document.querySelector('.lupa');
+    var close = document.querySelector('.close');
+    var search = document.querySelector('.search');
     
     // Adiciona o evento de clique ao botão de menu
-    btnMenu.addEventListener('click', function () {
-        menu.style.display = 'block';
+    lupa.addEventListener('click', function () {
+        search.style.display = 'block';
     });
     
     // Adiciona o evento de clique ao botão de fechar
-    btnClose.addEventListener('click', function () {
-        menu.style.display = 'none';
+    close.addEventListener('click', function () {
+        search.style.display = 'none';
     });
 });
